869aea3712b7a4208a39c64a8e8d8dab1f0bf521
Saurav-Kumar-02 <sauravkr811311@gmail.com>
2025-08-22 16:21:04 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,18 @@
+# Task 1:
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("\nResults of Mathematical Operations:")
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+# Task 2: 
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+full_name = first_name + " " + last_name
+
+print("\nHello,", full_name + "! Welcome to Python programming.")
